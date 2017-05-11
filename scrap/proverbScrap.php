9f989ad7800850537ed1837518ed6f62d63488ef
Adjib Ahamada <ahamada.adjib@gmail.com>
2017-05-11 12:03:13 +0200

    Srap
    Maj scrap

--- scrap/proverbScrap.php
+++ scrap/proverbScrap.php
@@ -1,31 +1,39 @@
 <?php
 
 /*Connexion BDD*/
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proverbdb";
 
+// Create connection
+$conn = new mysql($servername, $username, $password, $dbname);
 
-
-
-
-//sql_connect();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 $i = 1;
-while($i < 802){
-	$url = "http://www.unproverbe.com/les-proverbes.html?page=".$i."&p=".$i."&call=1";
+while($i < 100){
+	$url = "http://www.unproverbe.com/proverbes-amitie.html".$i."&p=".$i."&call=1";
 	$code_source_page = file_get_contents($url);
 	$tab_block = preg_split('#id=\"provNorm\"#',$code_source_page);
 	foreach($tab_block as $k => $v){
-		if(preg_match('#title=\"([^\"]+)\"#U',$v,$m))   
+		if(preg_match('#title=\"([^\"]+)\"#U',$v,$m))
 			$location = $m[1];
-		if(preg_match('#<strong>([^<]+)<\/strong>#',$v,$m)) 
+		if(preg_match('#<strong>([^<]+)<\/strong>#',$v,$m))
 			$categorie = $m[1];
-		if(preg_match('#class=\"flagProv\" >([^<]+)<\/a>#',$v,$m)) 
+		if(preg_match('#class=\"flagProv\" >([^<]+)<\/a>#',$v,$m))
 			$proverbe = $m[1];
 
 		print_r($tab_block);
-		//$req ="INSERT INTO";
+		$req ="INSERT INTO pages (urlPage) VALUES ('".$url."')";
+		$conn ->query($sql)
 		//$res =sql($req);
 	}
-$i++;	
+$i++;
 }
 
 //sql_close();
